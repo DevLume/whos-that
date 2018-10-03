1668f9d9e399b9b12067da2f3171fc828c8f33be
DevLume <lukaszm.dev@gmail.com>
2018-10-03 15:06:32 +0200

    pass reminder

--- ForgottenPasswordForm.cs
+++ ForgottenPasswordForm.cs
@@ -12,18 +12,24 @@ namespace Whos_that
 {
     public partial class ForgottenPasswordForm : Form
     {
+        String emailText;
         public ForgottenPasswordForm()
         {
             InitializeComponent();
         }
         private void forgottenPasword_TextChanged(object sender, EventArgs e)
         {
+            emailText = forgottenPasword.Text;
+
             forgottenPasword.ForeColor = Color.FromArgb(78, 184, 206);
             forgottenPasswordPanel.BackColor = Color.FromArgb(78, 184, 206);
         }
 
         private void forgottenPaswordSubmt_Click(object sender, EventArgs e)
         {
+            AccountManager acmos = new AccountManager();
+
+            acmos.RemindPassword(emailText);
 
             this.Close();
         }
