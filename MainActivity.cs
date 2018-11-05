7c304ecab97a9b64065a9a355295441095d76f33
cebukas <42977685+cebukas@users.noreply.github.com>
2018-11-05 20:49:55 +0200

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace Whos_that
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button btnRegister;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
+            btnRegister.Click += (object sender, System.EventArgs e) =>
+            {
+                Intent intent = new Intent(this, typeof(registerUI));
+                this.StartActivity(intent);
+            };
+        }
+    }
+}
