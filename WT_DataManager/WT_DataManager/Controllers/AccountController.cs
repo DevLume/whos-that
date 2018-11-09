7e689b270c5cc4c0b2efeb0a1ea06477c7fd26d1
DevLume <lukaszm.dev@gmail.com>
2018-11-09 12:37:23 +0200

    added database to webAPI

---
+++ WT_DataManager/WT_DataManager/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Whos_that;
+using WT_DataManager.Models;
+
+namespace WT_DataManager.Controllers
+{
+    public class AccountController : ApiController
+    {
+        
+        AccountManagerDB accman = new AccountManagerDB();
+        public AccountController()
+        { }
+
+
+        [AcceptVerbs("GET", "POST")]
+        [Route("api/Account")]
+        [HttpPost]
+        public HttpResponseMessage Register(string username, string password, string email)
+        {
+            bool result = false;
+            string responseMessage;
+            result = accman.CreateAccount(username, password, email, out responseMessage);
+            if (result)
+            {
+                return Request.CreateResponse(HttpStatusCode.Accepted, responseMessage);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.Accepted, responseMessage);
+            }
+        }
+
+        [Route("api/Account/5")]
+        [HttpGet]
+        public HttpResponseMessage Login(string username, string password)
+        {
+            bool result = false;
+            string responseMessage;
+            result = accman.Login(username, password, out responseMessage);
+            if (result)
+            {
+                return Request.CreateResponse(HttpStatusCode.Accepted, responseMessage);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.Accepted, responseMessage);
+            }
+        }
+    }
+}
--- WT_DataManager/WT_DataManager/Controllers/RWController.cs
+++ WT_DataManager/WT_DataManager/Controllers/RWController.cs
@@ -7,53 +7,15 @@ using System.Web.Http;
 using WT_DataManager.Models;
 
 namespace WT_DataManager.Controllers
-{
-    [RoutePrefix("api/Login")]
+{  
+    //Read-Write controller
     public class RWController : ApiController
     {
-        dataEntities db = new dataEntities();
-        // GET api/Login
-        public string getString(string hotdog)
+        [Route("api/delivery/Test")]
+        [HttpGet, HttpPost]
+        public HttpResponseMessage GetDeliveryItem(string testString = "api werkz")
         {
-            if (hotdog == "hotdog")
-            {
-                return "Hotdog!";
-            }
-            else
-            {
-                return "Not Hotdog!";
-            }
-        }
-
-        [HttpPost]
-        [Route("WEBREGISTER")]
-        //POST: API/Login
-        public HttpResponseMessage WebRegister(string username, string password)
-        {
-            //TODO: Rewrite this method to check for proper user data
-            usersTestTable uTable = new usersTestTable();
-            uTable.Name = username;
-            uTable.PassHash = password;
-            db.usersTestTables.Add(uTable);
-            db.SaveChanges();
-            return Request.CreateResponse(HttpStatusCode.Accepted, "Account created succesfully");
-        }
-
-        //GET API/Login
-        [HttpGet]
-        [Route("WEBLOGIN")]
-        public HttpResponseMessage WebLogin(string username, string password)
-        {
-            //TODO: Rewrite this method to check for proper user data
-            var user = db.usersTestTables.Where(x => x.Name == username && x.PassHash == password).FirstOrDefault();
-            if (user == null)
-            {
-                return Request.CreateResponse(HttpStatusCode.Unauthorized, "Please enter valid credentials");
-            }
-            else
-            {
-                return Request.CreateResponse(HttpStatusCode.Accepted, "Success");
-            }
+            return Request.CreateResponse<string>(HttpStatusCode.OK, testString);
         }
     }
 }
