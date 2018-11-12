1cca1dcbdaf427e8b6eeaf0655b774b44af986d0
DevLume <lukaszm.dev@gmail.com>
2018-11-12 22:06:50 +0200

    Added login/register logic

---
+++ Classes/GuiMessageManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Whos_that
+{
+    public class GuiMessageManager
+    {
+        public delegate void TransmitMessage(HttpResponseMessage m, Context con);
+
+        public void TransmitHttpMessage(HttpResponseMessage mesg, Context contxt)
+        {
+            var tmesg = mesg.Content.ReadAsStringAsync().Result.Replace("\\", "").Trim(new char[1]
+                        {
+                        '"'
+                        });
+            Toast.MakeText(contxt, tmesg, ToastLength.Long).Show();
+        }
+    }
+}
--- MainActivity.cs
+++ MainActivity.cs
@@ -6,7 +6,11 @@ using Android.Widget;
 using Android.Content;
 using Android.Views.InputMethods;
 using System;
+
 using Android.Views;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
 
 namespace Whos_that
 {
@@ -16,11 +20,19 @@ namespace Whos_that
         private Button btnRegister;
         LinearLayout linearLayout;
 
+        EditText usernameText;
+        EditText passwordText;
+        private Button btnLogin;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
-
+          
+            usernameText = FindViewById<EditText>(Resource.Id.txtLoginUsername);
+            passwordText = FindViewById<EditText>(Resource.Id.txtLoginPassword);
+            btnLogin = FindViewById<Button>(Resource.Id.btnSignIn);
+          
             btnRegister = FindViewById<Button>(Resource.Id.btnRegister);
             btnRegister.Click += (object sender, System.EventArgs e) =>
             {
@@ -28,8 +40,16 @@ namespace Whos_that
                 this.StartActivity(intent);
             };
 
+            btnLogin.Click += btnSignIn_Click;
+
             linearLayout = FindViewById<LinearLayout>(Resource.Id.loginLinearLayout);
             linearLayout.Click += LinearLayout_Click;
+        }
+
+        private void btnSignIn_Click(object sender, EventArgs e)
+        {
+            EntryManager entryman = new EntryManager();
+            entryman.SendLoginRequest(usernameText.Text, passwordText.Text, this);
         }
 
         private void LinearLayout_Click(object sender, EventArgs e)
