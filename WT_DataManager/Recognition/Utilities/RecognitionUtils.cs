798a74df947e61012aac825ef554e2527cfb972d
LukasElenbergas <lukas.elenbergas@gmail.com>
2018-11-13 12:44:25 +0200

    Implemented method for getting memory stream from URI

---
+++ WT_DataManager/Recognition/Utilities/RecognitionUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Recognition.Utilities
+{
+    public class RecognitionUtils
+    {
+        public WebClient WebClient { get; set; }
+
+        /// <summary>
+        /// Gets stream of image from URI
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        public MemoryStream GetStream(string uri)
+        {
+            WebClient = new WebClient();
+            byte[] imageData = null;
+
+            imageData = WebClient.DownloadData(uri);
+
+            return new MemoryStream(imageData);
+        }
+    }
+}
