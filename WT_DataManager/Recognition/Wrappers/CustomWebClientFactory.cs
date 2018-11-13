24f6af16177f82a039f207f33322d74ccb9193c7
LukasElenbergas <lukas.elenbergas@gmail.com>
2018-11-13 13:01:57 +0200

    Created custom WebClient generation for testing

---
+++ WT_DataManager/Recognition/Wrappers/CustomWebClientFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recognition.Wrappers
+{
+    public class CustomWebClientFactory : ICustomWebClientFactory
+    {
+        public ICustomWebClient Create()
+        {
+            return new CustomWebClient();   
+        }
+    }
+}
