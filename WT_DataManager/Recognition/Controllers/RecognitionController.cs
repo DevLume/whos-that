60d2bbc016298adcafd3335545b0a98cb27c7e95
LukasElenbergas <lukas.elenbergas@gmail.com>
2018-11-20 15:08:29 +0200

    Controller to call recognition method to create group

--- WT_DataManager/Recognition/Controllers/RecognitionController.cs
+++ WT_DataManager/Recognition/Controllers/RecognitionController.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Recognition.RecognitionServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Recognition.Controllers
 {
-    public class RecognitionController
+    [Route("api/[controller]")]
+    public class RecognitionController : Controller
     {
+        private RecServices _recServices { get; set; } = new RecServices();
+
+        [HttpGet]
+        [Route("creategroup")]
+        public async Task<ActionResult> CreateGroup()
+        {
+            return Ok(await _recServices.CreateGroup());
+        }
     }
 }
